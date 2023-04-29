b485125ea7881907a43ff59062de30c720715a22
Fatih <afeserr@gmail.com>
2023-04-29 21:40:00 +0200

    update coverage test

--- auto_coverage_calculator.sh
+++ auto_coverage_calculator.sh
@@ -4,9 +4,15 @@ report_file=/home/afeser/ansible2/test/results/reports/coverage/index.html
 
 for test in $module_names
 do
-    echo "Running tests for $test"
-    ansible-test coverage erase
-    ansible-test units --coverage $test --docker
-    ansible-test coverage html
-    mv $report_file coverage_result_$test.html
+    if [ -z "coverage_result_$test.html" ]
+    then
+        echo "Found already an existing result. Please remove it first if you wish to run again: coverage_result_$test.html"
+    else
+        echo "Running tests for $test"
+        ansible-test coverage erase
+        ansible-test units --coverage $test --docker
+        ansible-test coverage html
+        mv $report_file coverage_result_$test.html
+    fi
+
 done
