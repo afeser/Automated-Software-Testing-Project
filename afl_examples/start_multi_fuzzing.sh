720046bd13f01f951fa809832bdfca77ae9c4ef2
afeser <afeserr@gmail.com>
2023-06-02 00:19:23 +0200

    sorry env variable not available

--- afl_examples/start_multi_fuzzing.sh
+++ afl_examples/start_multi_fuzzing.sh
@@ -2,7 +2,7 @@ module_name=iptables
 num_cores=8
 
 # start master anyway...
-tmux new-session -d -s $module_name.01master "source ~/.bashrc; py-afl-fuzz -m 5000  -i ./inputs/$module_name/inputs -o outputs/$module_name -M fuzzer0 -- python3 ./wrapper-modules.py  @@ $module_name"
+tmux new-session -d -s $module_name.01master "source /home/afeser/.bashrc; py-afl-fuzz -m 5000  -i ./inputs/$module_name/inputs -o outputs/$module_name -M fuzzer0 -- python3 ./wrapper-modules.py  @@ $module_name"
 sleep 1
 
 end=$(($num_cores-1))
@@ -11,7 +11,7 @@ start=1
 for (( i=$start; i<=$end; i++ ))
 do
     echo "Starting fuzzer $i"
-    tmux new-session -d -s $module_name.$i.slave "source ~/.bashrc; py-afl-fuzz -m 5000  -i ./inputs/$module_name/inputs -o outputs/$module_name -S fuzzer$i -- python3 ./wrapper-modules.py  @@ $module_name"
+    tmux new-session -d -s $module_name.$i.slave "source /home/afeser/.bashrc; py-afl-fuzz -m 5000  -i ./inputs/$module_name/inputs -o outputs/$module_name -S fuzzer$i -- python3 ./wrapper-modules.py  @@ $module_name"
     sleep 1
 done
 
