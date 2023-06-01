3a436e7db71166e1fd69b0f65993e25c511a65b2
afeser <afeserr@gmail.com>
2023-06-02 00:13:40 +0200

    start for 8 cores

--- afl_examples/start_multi_fuzzing.sh
+++ afl_examples/start_multi_fuzzing.sh
@@ -1,14 +1,27 @@
-module_name=apt
+module_name=iptables
+num_cores=8
 
-echo "Starting fuzzer 1"
-tmux new-session -d -s $module_name.01master "py-afl-fuzz -m 5000  -i ./inputs/$module_name/inputs -o outputs/$module_name -M fuzzer01 -- python3 ./wrapper-modules.py  @@ $module_name"
+# start master anyway...
+tmux new-session -d -s $module_name.01master "py-afl-fuzz -m 5000  -i ./inputs/$module_name/inputs -o outputs/$module_name -M fuzzer0 -- python3 ./wrapper-modules.py  @@ $module_name"
 sleep 1
-echo "Starting fuzzer 2"
-tmux new-session -d -s $module_name.02slave "py-afl-fuzz -m 5000  -i ./inputs/$module_name/inputs -o outputs/$module_name -S fuzzer02 -- python3 ./wrapper-modules.py  @@ $module_name"
-sleep 1
-echo "Starting fuzzer 3"
-tmux new-session -d -s $module_name.03slave "py-afl-fuzz -m 5000  -i ./inputs/$module_name/inputs -o outputs/$module_name -S fuzzer03 -- python3 ./wrapper-modules.py  @@ $module_name"
-sleep 1
-echo "Starting fuzzer 4"
-tmux new-session -d -s $module_name.04slave "py-afl-fuzz -m 5000  -i ./inputs/$module_name/inputs -o outputs/$module_name -S fuzzer04 -- python3 ./wrapper-modules.py  @@ $module_name"
+
+end=$(($num_cores-1))
+start=1 
+# start slaves
+for (( i=$start; i<=$end; i++ ))
+do
+    echo "Starting fuzzer $i"
+    tmux new-session -d -s $module_name.$i.slave "py-afl-fuzz -m 5000  -i ./inputs/$module_name/inputs -o outputs/$module_name -S fuzzer$i -- python3 ./wrapper-modules.py  @@ $module_name"
+    sleep 1
+done
+
+
+# echo "Starting fuzzer 2"
+# tmux new-session -d -s $module_name.02slave "py-afl-fuzz -m 5000  -i ./inputs/$module_name/inputs -o outputs/$module_name -S fuzzer02 -- python3 ./wrapper-modules.py  @@ $module_name"
+# sleep 1
+# echo "Starting fuzzer 3"
+# tmux new-session -d -s $module_name.03slave "py-afl-fuzz -m 5000  -i ./inputs/$module_name/inputs -o outputs/$module_name -S fuzzer03 -- python3 ./wrapper-modules.py  @@ $module_name"
+# sleep 1
+# echo "Starting fuzzer 4"
+# tmux new-session -d -s $module_name.04slave "py-afl-fuzz -m 5000  -i ./inputs/$module_name/inputs -o outputs/$module_name -S fuzzer04 -- python3 ./wrapper-modules.py  @@ $module_name"
 
